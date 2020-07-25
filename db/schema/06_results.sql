8df583e20ca54a60f60d0862324f1d3ca6d6f5f3
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-25 14:49:18 +0000

    result integer instead of varchar

--- db/schema/06_results.sql
+++ db/schema/06_results.sql
@@ -5,6 +5,6 @@ CREATE TABLE results (
   quiz_id INTEGER NOT NULL REFERENCES quizzes(id) ON DELETE CASCADE,
   date_taken DATE NOT NULL,
   share_link VARCHAR(255) NOT NULL,
-  result VARCHAR(255) NOT NULL,
+  result INTEGER NOT NULL,
   description TEXT NOT NULL
 );
