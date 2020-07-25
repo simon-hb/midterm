ed3b7c8506aeb86391895dde627dd190285971db
Simon Jung <simon.hb.jung@hotmail.com>
2020-07-25 20:11:29 +0000

    updated schemas to current erd

---
+++ db/schema/03_quiz_questions.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS quiz_questions CASCADE;
+CREATE TABLE trivia_questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER NOT NULL REFERENCES quizzes(id) ON DELETE CASCADE,
+  question_number SMALLINT NOT NULL,
+  question text NOT NULL,
+  question_type VARCHAR(255) NOT NULL
+);
---
+++ db/schema/04_question_options.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS question_options CASCADE;
+CREATE TABLE question_options (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER NOT NULL REFERENCES quizzes(id) ON DELETE CASCADE,
+  quiz_question_id INTEGER NOT NULL REFERENCES quiz_questions(id) ON DELETE CASCADE,
+  answer TEXT NOT NULL,
+  order SMALLINT NOT NULL,
+  is_correct BOOLEAN NOT NULL DEFAULT false
+);
