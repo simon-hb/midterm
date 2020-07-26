b8c383fb8485c708fea7e502c85dbead11a954bd
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-26 22:47:04 +0000

    sets cookie on login

--- routes/login.js
+++ routes/login.js
@@ -6,69 +6,39 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-// checks to see if user exists and returns the user object if found or falsy value
-const findUser = (emailID, users) => {
-  for (const userID in users) {
-    const encodedEmail = htmlEncode(users[userID].email);
-    if ( encodedEmail === emailID) {
-      return users[userID];
-    }
-  }
-  return false;
-}
-
-
-// validates user's email and password combination and return truthy/falsy
-const validatePassword = (userObj, email, passwordToCheck) => {
-  console.log("UTC:", userObj);
-  console.log("PWTC:", passwordToCheck);
-  console.log("ETC:", email);
-  const encodedUserEmail = htmlEncode(userObj.email);
-  if ( encodedUserEmail === email && passwordToCheck === userObj.password) {
-    return true;
-  }
-  return false;
-}
-
-// finds urls associated with a given userID
-const findURLSByUser = (userCookieID, urlDatabase) => {
-  let usersURLs = {};
-  for (const url in urlDatabase) {
-    if (urlDatabase[url].userIDforLink === userCookieID) {
-      usersURLs[url] = urlDatabase[url];
-      usersURLs[url].longURL = urlDatabase[url].longURL
-    }
-  }
-  return usersURLs;
-}
-
+const { findUser, validatePassword } = require("../helpers");
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    console.log("inside login route:",req.body);
-    
+    console.log("inside login route:", req.body);
+
     db.query(`SELECT * FROM users;`)
       .then(data => {
         const users = data.rows;
         const checkUser = findUser(req.body.email, users);
-        const emailPasswordCheck = validatePassword(checkUser, req.body.email, req.body.password);
-        
-        res.json({ users });
+        if (checkUser) {
+          console.log("CU", checkUser);
+          
+          const emailPasswordCheck = validatePassword(checkUser, req.body.email, req.body.password);
+          console.log("CP", emailPasswordCheck);
+
+          if (emailPasswordCheck) {
+            // let userID = checkUser.id;
+            console.log(users);
+            req.session.user_id = checkUser.id;
+            res.redirect("/");
+          }
+        } 
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       })
-    });
+  });
 
   return router;
 };
 
-function htmlEncode(str) {
-  return String(str).replace(/[^\w. ]/gi, function (c) {
-    return '&#' + c.charCodeAt(0) + ';';
-  });
-}
