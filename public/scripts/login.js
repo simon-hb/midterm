703b7a8f0b9163681bab77e1d03096327aa0a6ac
NatalieSokolova <nataliesklv@gmail.com>
2020-07-27 21:44:39 +0000

    homepage styling (colored placeholder imgs, added redirect links)

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
-$( document ).ready(function() {
-  console.log( "ready!" );
-=======
 $(document).ready(function () {
 
   $("#login-form-div").css("display", "none")
->>>>>>> staging
 
   // SHOW/HIDE LOGIN FORM
   const showHideLogin = () => {
@@ -26,13 +21,8 @@ $(document).ready(function () {
     const email = htmlEncode(this.email.value); // escapes malicious code
     const password = this.password.value; // escapes malicious code
 
-<<<<<<< HEAD
-    const userLoginData = {email,password};
-    $.ajax({
-=======
     const userLoginData = { email, password };
     const ajaxReq = $.ajax({
->>>>>>> staging
       type: "POST",
       url: "/login",
       data: userLoginData,
@@ -40,7 +30,7 @@ $(document).ready(function () {
       .done(function (output, status) {
         const userValidatedHeader = ajaxReq.getResponseHeader('userValidated') === "true";
         const passwordValidatedHeader = ajaxReq.getResponseHeader('passwordValidated') === "true";
-        
+
         console.log()
 
         if (userValidatedHeader && passwordValidatedHeader) {
@@ -52,16 +42,8 @@ $(document).ready(function () {
           alert("GET YOUR STUFF TOGETHER!")
         }
       })
-<<<<<<< HEAD
-      .fail(function(error) {
-        console.log( "error", error );
-      })
-
-  });
-=======
       .fail(function (error) {
         console.log("error", error);
->>>>>>> staging
 
       });
 
