33b44cfdf6dca465fadf8a195e024290923421de
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-30 05:05:00 +0000

    template form to create quiz

--- public/scripts/makequiz.js
+++ public/scripts/makequiz.js
@@ -1,21 +1,57 @@
 $(() => {
   let questionNumber = 2;
-  let optionNumber = 5;
+  let optionNumber = 1;
 
-  $("#add-question").click(()=> {
+  const createQuestionElement = () => {
+
+    const questionElement = `
+          <div class="card mb-3">
+            <div class="card-body">
+              <div>
+                <input name="question${questionNumber}-title" class="form-control form-control-lg" type="text"
+                  placeholder="Title for Question ${questionNumber}" required>
+              </div>
+              <div>
+                <input class="mt-3 form-control custom-success" type="text" name="question_${questionNumber}-option_${optionNumber}"
+                  placeholder="Enter your correct answer here" required>
+                <input class="mt-3 form-control" type="text" name="question_${questionNumber}-option_${optionNumber+1}"
+                  placeholder="Enter an incorrect answer here" required>
+                <input class="mt-3 form-control" type="text" name="question_${questionNumber}-option_${questionNumber+2}"
+                  placeholder="Enter an incorrect answer here" required>
+                <input class="mt-3 form-control" type="text" name="question_${questionNumber}-option_${questionNumber+3}"
+                  placeholder="Enter an incorrect answer here" required>
+              </div>
+
+            </div>
+          </div> 
+    `;
+
+    questionNumber += 1;
+    optionNumber += 4
+    return questionElement;
+
+  }
+
+  $("#add-question").click(() => {
 
     $("#questions").append(createQuestionElement())
   })
-  
 
 
+  $('#makequiz').submit((event) => {
+    event.preventDefault()
+    const queryString = $('#makequiz').serializeArray();
+
+    console.log(queryString);
+  })
 
 
 
 
 }); // document ready
 
-function textAreaAdjust(o) {
-  o.style.height = "1px";
-  o.style.height = (25+o.scrollHeight)+"px";
-}
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
